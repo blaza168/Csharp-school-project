48932d408ba1e72a9357c083c51158c8b23ba3a2
blaza168 <jan.blazek10@gmail.com>
2021-02-12 16:36:24 +0100

    Migrate to razor pages

--- Shop.Database/ApplicationDbContext.cs
+++ Shop.Database/ApplicationDbContext.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Linq;
+using System.Net.Sockets;
 using Microsoft.EntityFrameworkCore;
 using Shop.Domain.Models;
 
 namespace Shop.Database
 {
+    
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Product> Products { get; set; }

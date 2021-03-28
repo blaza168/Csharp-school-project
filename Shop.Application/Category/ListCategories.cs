baf0cd1bfd0be6aa0852cbb126e3d25feda4c998
MartinD-CZ <martin@embedblog.eu>
2021-03-28 10:59:18 +0200

    added endpoint for listing all categories

---
+++ Shop.Application/Category/ListCategories.cs
@@ -0,0 +1,22 @@
+﻿using Shop.Application.Category.ViewModels;
+using Shop.Database.Repositories;
+using System.Collections.Generic;
+
+namespace Shop.Application.Category
+{
+    [Service]
+    public class ListCategories
+    {
+        private readonly CategoryRepository _categoryRepository;
+
+        public ListCategories(CategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IEnumerable<CategoryViewModel> Do()
+        {
+            return _categoryRepository.GetCategories(selector: GetCategory.CategoryMapper);
+        }
+    }
+}
--- WebApplication/Controllers/CategoryController.cs
+++ WebApplication/Controllers/CategoryController.cs
@@ -19,11 +19,11 @@ namespace WebApplication.Controllers
             return BadRequest();
         }
 
-        /*[HttpGet("list")]
+        [HttpGet("list")]
         public IActionResult List([FromServices] ListCategories listCategories)
         {
             return Json(listCategories.Do());
-        }*/
+        }
 
         [HttpGet("{categoryId}")]
         public IActionResult GetCategory([FromServices] GetCategory getCategory, int categoryId)
